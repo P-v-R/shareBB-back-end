27c84e824f9414696439f9cd18bdc5d427eb43ab
Perryvon <Perry.j.vr@gmail.com>
2021-06-08 11:43:33 -0700

    restarting adding dependencies

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-/** Express app for jobly. */
+/** Express app for ShareBB. */
 
 const express = require("express");
 const cors = require("cors");
@@ -8,10 +8,10 @@ const cors = require("cors");
 const { NotFoundError } = require("./expressError");
 
 const { authenticateJWT } = require("./middleware/auth");
-const authRoutes = require("./routes/auth");
-const companiesRoutes = require("./routes/companies");
-const usersRoutes = require("./routes/users");
-const jobsRoutes = require("./routes/jobs");
+// const authRoutes = require("./routes/auth");
+// const companiesRoutes = require("./routes/companies");
+// const usersRoutes = require("./routes/users");
+// const jobsRoutes = require("./routes/jobs");
 
 const morgan = require("morgan");
 
@@ -22,10 +22,10 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
 
-app.use("/auth", authRoutes);
-app.use("/companies", companiesRoutes);
-app.use("/users", usersRoutes);
-app.use("/jobs", jobsRoutes);
+// app.use("/auth", authRoutes);
+// app.use("/companies", companiesRoutes);
+// app.use("/users", usersRoutes);
+// app.use("/jobs", jobsRoutes);
 
 
 /** Handle 404 errors -- this matches everything */
