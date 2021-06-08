5f01868420658ac43e164331660b1baabf9ccdea
momes <mo.enokida@gmail.com>
2021-06-08 12:25:35 -0700

    database seeded

---
+++ sharebnb-schema.sql
@@ -0,0 +1,52 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL UNIQUE
+    CHECK (position('@' IN email) > 1),
+  password TEXT NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE listings (
+  id SERIAL PRIMARY KEY,
+  address VARCHAR(50) NOT NULL,
+  unit VARCHAR(10),
+  city VARCHAR(30) NOT NULL,
+  state VARCHAR(2) NOT NULL,
+  zip INTEGER NOT NULL,
+  country VARCHAR(3) NOT NULL,
+  owner_id INTEGER REFERENCES users,
+  title VARCHAR(50) NOT NULL,
+  description TEXT NOT NULL,
+  photo_url TEXT,
+  price_per_hour NUMERIC CHECK (price_per_hour >= 0) NOT NULL,
+  min_hours INTEGER NOT NULL
+);
+
+CREATE TABLE bookings (
+  id SERIAL PRIMARY KEY,
+  listing_id INTEGER REFERENCES listings,
+  renter_id INTEGER REFERENCES users,
+  start_date DATE NOT NULL,
+  num_hours INTEGER NOT NULL,
+  total_price NUMERIC NOT NULL,
+  booked_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE messages (
+  listing_id INTEGER REFERENCES listings,
+  from_user_id INTEGER REFERENCES users,
+  to_user_id INTEGER REFERENCES users,
+  message TEXT NOT NULL,
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tags (
+  name VARCHAR(20) PRIMARY KEY
+);
+
+CREATE TABLE listings_to_tags (
+  listing_id INTEGER REFERENCES listings,
+  tag VARCHAR(20) REFERENCES tags
+);
---
+++ sharebnb-seed.sql
@@ -0,0 +1,63 @@
+-- both test users have the password "password"
+
+INSERT INTO users (first_name, last_name, email, password, is_admin)
+VALUES ('mo',
+        'enokida',
+        'mo@mo.com',
+        '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+        FALSE),
+       ('perry',
+        'von',
+        'perry@perry.com',
+        '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+        TRUE);
+
+INSERT INTO listings (address, 
+                        unit,
+                        city,
+                        state,
+                        zip,
+                        country,
+                        owner_id,
+                        title,
+                        description,
+                        photo_url,
+                        price_per_hour,
+                        min_hours
+                        )
+VALUES ('123 test st', 'A', 'Los Angeles', 'CA', 90064, 'USA', 1, 'mo test listing', 'this is my listing description',
+        NULL, 50.50 , 2),
+        ('1111 test ave', '2B', 'Los Angeles', 'CA', 90010, 'USA', 2, 'perry test listing', 'this is my listing description',
+        NULL, 100, 5);
+
+INSERT INTO bookings (listing_id, renter_id, start_date, num_hours, total_price)
+VALUES (2, 1, '2021-07-20', 4, 400),
+       (2, 1, '2021-07-21', 2, 200),
+       (1, 2, '2021-07-10', 1, 50.50);
+
+
+
+INSERT INTO messages (listing_id, from_user_id, to_user_id, message)
+VALUES (1, 2, 1, 'from p to m'),
+       (1, 1, 2, 'from m to p yes its available'),
+       (2, 1, 2, 'from m to p test');
+
+
+INSERT INTO tags (name)
+VALUES ('Swimming Pool'),
+       ('Hot Tub'),
+       ('Sauna'),
+       ('BBQ/Grill'),
+       ('Yard'),
+       ('Fire Pit'),
+       ('Gym/Workout'),
+       ('Trampoline'),
+       ('Garden'),
+       ('Music Studio'),
+       ('Patio');
+
+INSERT INTO listings_to_tags (listing_id, tag)
+VALUES (1, 'Swimming Pool'),
+        (1, 'Yard'),
+        (2, 'Trampoline'),
+        (2, 'BBQ/Grill');
